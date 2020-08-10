d3d290492faad00474b316cdb3377f61a9388262
Brian Longmore <brianl.ext@gmail.com>
2020-08-09 19:22:26 -1000

    got it workin

--- server/main.go
+++ server/main.go
@@ -7,8 +7,12 @@ import (
 func main() {
 	server := fiber.New()
 
-	server.Get('/', func(c *fiber.Ctx) {
+	server.Get("/", func(c *fiber.Ctx) {
 		c.Send("bruh what are you doin")
+	})
+
+	server.Get("*", func(c *fiber.Ctx) {
+		c.Send("nothing to see here")
 	})
 
 	server.Listen(3000)
